fa2efc13b084558021162c29d0bff75b4a5402c5
jeyaseelan <jeyaseelan@handbuiltapps.com>
2025-05-04 09:21:29 +0530

    Add TextWithImage

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,38 +1,13 @@
 import React from 'react';
-import Section from '../../Components/Section';
 
-import { Link } from 'react-router-dom';
-import earth from '../../assets/earth.png';
-
-import style from './style.module.css';
+import TextWithImage from '../../Components/Widgets/TextWithImage';
 
 const Home = () => {
-  return (
-    <Section>
-      <div className={style.container}>
-        <div className={style.col50}>
-          <h1 className={style.heading}>
-            Reach the <br />
-            Heart of your <br />
-            Target Audience
-          </h1>
-
-          <p className={style.text}>Start a free 30-Minute Strategy chat</p>
-
-          <Link className={style.button} to={"#"}>
-            Start Now
-          </Link>
-
-        </div>
-        <div className={style.col50}>
-          <div className={style.imageContainer}>
-            <div className={style.sizer}></div>
-            <img src={earth} />
-          </div>
-        </div>
-      </div>
-    </Section>
-  )
+    return (
+        <>
+            <TextWithImage />
+        </>
+    )
 }
 
 export default Home;
